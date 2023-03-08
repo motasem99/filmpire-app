cfd51929b195a97b584420c508f8c86fa7b8e1b0
mutasem <motasemimad10@gmail.com>
2023-03-08 16:55:14 +0200

    Component Structure & Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
+import { Movies, Actors, MovieInfo, Navbar, Profile } from './components/index';
 
 const App = () => {
   return (
     <div className='App'>
       <CssBaseline />
-      <h1>Hello App</h1>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route exact path='/' element={<Movies />} />
+          <Route exact path='/movie/:id' element={<MovieInfo />} />
+          <Route exact path='/actors/:id' element={<Actors />} />
+          <Route exact path='/profile/:id' element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 };
