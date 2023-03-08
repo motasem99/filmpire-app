dbf61518043034997e04ebf1d728a63e30ca2568
mutasem <motasemimad10@gmail.com>
2023-03-08 16:25:32 +0200

    intro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>Hello React</h1>
+      <CssBaseline />
+      <h1>Hello App</h1>
     </div>
   );
 };
